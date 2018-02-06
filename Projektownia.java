b62271dbc70cabd71abb806d507a05de23efcd83
Aleksander Lorenc <zedlord89@gmail.com>
2018-02-06 19:57:03 +0100

    added queues and person class

--- Projektownia.java
+++ Projektownia.java
@@ -6,6 +6,8 @@ import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.util.concurrent.Semaphore;
+import java.util.Random;
 
 class Projektownia extends JFrame
 {
@@ -23,6 +25,8 @@ private JLabel newProjectSizeLabel = new JLabel("# of people needed:");
 private JLabel newProjectTimeLabel = new JLabel("Required time(in s):");
 private JButton newEmploeeButton = new JButton("Spawn");
 private JButton newProjectButton = new JButton("Construct project");
+private Semaphore oldEmploeeApplications  = new Semaphore(0, true);
+private Semaphore candidateApplications = new Semaphore(0, true);
 
 Projektownia()
 	{
@@ -55,9 +59,76 @@ Projektownia()
 	projectSouthPanel.add(newProjectButton);
 	}
 
+Semaphore getOldEmploeeQueue()
+	{
+	return oldEmploeeApplications;
+	}
+
 public static void main(String[] args)
 	{
 	Projektownia mainWindow = new Projektownia();
 	mainWindow.setVisible(true);
+	}
+}
+
+class Person extends Thread
+{
+String name;
+Projektownia employer;
+Semaphore queue;
+Boolean assigned = false;
+
+Person(String id, Projektownia company, Semaphore offer)
+	{
+	name = id;
+	employer = company;
+	queue = offer;
+	}
+
+public void run()
+	{
+	while(true)
+		{
+		try
+			{
+			queue.acquire(); //apply for the job
+			}
+		catch(InterruptedException e)
+			{
+			return;
+			}
+		while(!assigned)
+			{
+			try
+				{
+				sleep(100); //wait for assignment
+				}
+			catch(InterruptedException e){}
+			}
+		Random rand = new Random();
+		while(assigned)
+			{
+			try
+				{
+				sleep(900); //wait to complete assignment
+				}
+			catch(InterruptedException e){}
+			if(rand.nextInt(100) < 11) //one in ten resigns during project
+				{
+				assigned = false;
+				return;
+				}
+			}
+		if(rand.nextInt(100) < 50) //half stays for another project
+			{
+			return;
+			}
+		queue = employer.getOldEmploeeQueue(); //look for a new project
+		}
+	}
+
+String fetchName()
+	{
+	return name;
 	}
 }
