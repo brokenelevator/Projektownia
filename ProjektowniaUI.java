5c113bb531a1b78b9e50ec8255edf238b18bb8c3
Aleksander Lorenc <zedlord89@gmail.com>
2018-02-07 03:59:00 +0100

    logic for construct project button. project class support for assembling and maintaining a team. person class fields are now private. temporaily disabled resigning during project.

--- ProjektowniaUI.java
+++ ProjektowniaUI.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.util.concurrent.Semaphore;
 import java.util.Random;
 import java.util.LinkedList;
+import java.util.Iterator;
 
 class ProjektowniaUI extends JFrame
 {
@@ -60,6 +61,7 @@ ProjektowniaUI()
 	projectNorthPanel.add(newProjectTimeLabel);
 	projectNorthPanel.add(newProjectTimeTextField);
 	projectSouthPanel.add(newProjectButton);
+	newProjectButton.addActionListener(new ConjureProject());
 	}
 
 class ConjurePeople implements ActionListener
@@ -79,6 +81,27 @@ class ConjurePeople implements ActionListener
 			peopleCount++;
 			new Person("Person " + peopleCount, company, company.getCandidateQueue()).start();
 			}
+		}
+	}
+
+class ConjureProject implements ActionListener
+	{
+	short projectCount = 0;
+	
+	public void actionPerformed(ActionEvent event)
+		{
+		if(!newProjectSizeTextField.getText().matches("^[0-9]{1,3}$")) //input consists of 1-3 digits
+			{
+			newProjectSizeTextField.setText("0");
+			return;
+			}
+		if(!newProjectTimeTextField.getText().matches("^[0-9]{1,3}$")) //input consists of 1-3 digits
+			{
+			newProjectTimeTextField.setText("0");
+			return;
+			}
+		projectCount++;
+		new Project("Project " + projectCount, Short.parseShort(newProjectTimeTextField.getText()), Short.parseShort(newProjectSizeTextField.getText()), company).start();
 		}
 	}
 
@@ -113,9 +136,9 @@ LinkedList<Person> getAvailableEmploeesList()
 
 class Person extends Thread
 {
-String name;
-Projektownia employer;
-Semaphore queue;
+private String name;
+private Projektownia employer;
+private Semaphore queue;
 Boolean assigned = false;
 
 Person(String id, Projektownia company, Semaphore offer)
@@ -155,11 +178,11 @@ public void run()
 				sleep(900); //wait to complete assignment
 				}
 			catch(InterruptedException e){}
-			if(rand.nextInt(100) < 11) //one in ten resigns during project
-				{
-				assigned = false;
-				return;
-				}
+			// if(rand.nextInt(100) < 11) //one in ten resigns during project
+				// {
+				// assigned = false;
+				// return;
+				// }
 			}
 		if(rand.nextInt(100) < 50) //half stays for another project
 			{
@@ -172,5 +195,88 @@ public void run()
 String fetchName()
 	{
 	return name;
+	}
+}
+
+class Project extends Thread
+{
+private String name;
+private short timeRequired;
+private short peopleRequired;
+private Projektownia company;
+private LinkedList<Person> team = new LinkedList<Person>();
+
+Project(String id, short time, short people, Projektownia owner)
+	{
+	name = id;
+	timeRequired = time;
+	peopleRequired = people;
+	company = owner;
+	}
+
+public void run()
+	{
+	Semaphore oldEmploeeList = company.getOldEmploeeQueue();
+	Semaphore candidateList = company.getCandidateQueue();
+	while(true)
+		{
+		System.out.println(name + ". Prepare for liftoff.");
+		if(team.size() == peopleRequired)
+			{
+			for(Iterator<Person> it = team.iterator(); it.hasNext();)
+				{
+				Person current = it.next();
+				if(current == null)
+					{
+					it.remove();
+					System.out.println("Null - removing: " + current.fetchName());
+					}
+				if(!current.assigned)
+					{
+					it.remove();
+					System.out.println("Unassigned - removing: " + current.fetchName());
+					}
+				}
+			}
+		while(team.size() < peopleRequired)
+			{
+			if(oldEmploeeList.hasQueuedThreads())
+				{
+				oldEmploeeList.release();
+				try
+					{
+					sleep(5);
+					}
+				catch(InterruptedException e){}
+				Person current = company.getAvailableEmploeesList().pollFirst();
+				current.assigned = true;
+				team.addLast(current);
+				continue;
+				}
+			if(candidateList.hasQueuedThreads())
+				{
+				candidateList.release();
+				try
+					{
+					sleep(5);
+					}
+				catch(InterruptedException e){}
+				Person current = company.getAvailableEmploeesList().pollFirst();
+				current.assigned = true;
+				team.addLast(current);
+				continue;
+				}
+			else
+				{
+				try
+					{
+					sleep(500);
+					}
+				catch(InterruptedException e){}
+				}
+			}
+		System.out.println(name + ". LIFTOFF.");
+		return;
+		}
 	}
 }
